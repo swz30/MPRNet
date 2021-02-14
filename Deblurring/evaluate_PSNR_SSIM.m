dc10a40667e022d63b4505b3263caafaeee7bd25
Syed Waqas Zamir <41057533+swz30@users.noreply.github.com>
2021-02-14 14:53:53 +0400

    Update evaluate_PSNR_SSIM.m

--- Deblurring/evaluate_PSNR_SSIM.m
+++ Deblurring/evaluate_PSNR_SSIM.m
@@ -1,3 +1,7 @@
+%% Multi-Stage Progressive Image Restoration
+%% Syed Waqas Zamir, Aditya Arora, Salman Khan, Munawar Hayat, Fahad Shahbaz Khan, Ming-Hsuan Yang, and Ling Shao
+%% https://arxiv.org/abs/2102.02808
+
 close all;clear all;
 
 datasets = {'GoPro'};
